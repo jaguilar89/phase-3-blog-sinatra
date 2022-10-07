99603b5bf7c3b04e0559ba82411daaed4f76d5b0
Jose Aguilar <jaguilarny@gmail.com>
2022-10-07 14:53:04 -0400

    do migration for comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_184534) do
+ActiveRecord::Schema.define(version: 2022_10_07_185031) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "body"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
