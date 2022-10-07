15d31f9af526be38586c0ac830e56de70ff0b274
Jose Aguilar <jaguilarny@gmail.com>
2022-10-07 17:22:55 -0400

    create initial routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  # Add your routes here
+  set :default_content_type, "application/json"
+
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/posts" do
+    posts = Post.all
+    posts.to_json
+  end
+
+  get "/posts/:id" do
+    post = Post.find(params[:id])
+    post.to_json
+  end
 end
